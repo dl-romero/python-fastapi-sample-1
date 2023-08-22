18d17963b0d668e2d7025e3eb05f532109d66c60
David Romero <dl_romero@outlook.com>
2023-08-22 13:47:11 -0700

    uploading

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python-docker-sample-1
+
+Uses Python, Fast API as a Resume sample.
+See [screenshot.pdf](screenshot.pdf) to view the end result of the app hosted locally.
---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+
+tags_metadata = [
+        {
+            "name": "About",
+            "description": "Operations with users. The **login** logic is also here.",
+        },
+        {
+            "name": "Locations",
+            "description": "Operations with users. The **login** logic is also here.",
+        }
+    ]
+
+app = FastAPI(
+    title="API Documentation",
+    description="Sample API Documentation using FastAPI and Python",
+    summary="Contains Dummy Data.",
+    version="0.0.1",
+    contact={
+        "name": "David Romero",
+        "url": "https://dromero.dev",
+        "email": "dl_romero@outlook.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    openapi_tags=tags_metadata
+    )
+
+@app.get("/about/", tags=["About"])
+async def about():
+    return ({"About": "I am an experienced Data Center Engineer with strong skills in maintaining global data centers and servers with a passion for automation and team leadership."})
+
+@app.get("/locations/", tags=["Locations"])
+async def locations():
+    return ({"Oregon": "All Locations", "California": "San Diego County"})
+
+# Run - python -m uvicorn main:app --reload
